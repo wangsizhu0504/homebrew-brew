f3f50a9a0dd9d5529dc44e0c098d8145d8cb67e8
kriszu <39726513+wangsizhu0504@users.noreply.github.com>
2024-04-23 10:21:27 +0800

    chore: First release

---
+++ Formula/nvc.rb
@@ -0,0 +1,14 @@
+# Documentation: https://docs.brew.sh/Formula-Cookbook
+#                https://rubydoc.brew.sh/Formula
+# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
+class Nvc < Formula
+  desc "A self-contained version control tool for Node.JS"
+  homepage "https://github.com/wangsizhu0504/nvc"
+  url "https://github.com/wangsizhu0504/nvc/releases/download/v0.1.0/nvc-macos.zip"
+  sha256 "7435039737dc41cbadcc290ea9b6dfa86e961dee28bdc4310db0149e21c3ae27"
+  version "0.1.0"
+
+  def install
+    bin.install "nvc"
+  end
+end
