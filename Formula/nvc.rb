a63680bcf1c1116ee64d186b6f21a9b5e90c5890
Kriszu <39726513+wangsizhu0504@users.noreply.github.com>
2024-12-15 23:58:02 -0600

    Update nvc.rb

--- Formula/nvc.rb
+++ Formula/nvc.rb
@@ -4,9 +4,9 @@
 class Nvc < Formula
   desc "A self-contained version control tool for Node.JS"
   homepage "https://github.com/wangsizhu0504/nvc"
-  url "https://github.com/wangsizhu0504/nvc/releases/download/v0.1.0/nvc-macos.zip"
+  url "https://github.com/wangsizhu0504/nvc/releases/download/v0.2.0/nvc-macos.zip"
   sha256 "7435039737dc41cbadcc290ea9b6dfa86e961dee28bdc4310db0149e21c3ae27"
-  version "0.1.0"
+  version "0.2.0"
 
   def install
     bin.install "nvc"
