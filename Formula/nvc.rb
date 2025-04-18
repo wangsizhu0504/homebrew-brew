1b681ffc765b7bdd816a19f1c15c372f3a24f8fa
kriszu <39726513+wangsizhu0504@users.noreply.github.com>
2025-04-18 10:34:42 +0800

    fix: update

--- Formula/nvc.rb
+++ Formula/nvc.rb
@@ -6,22 +6,21 @@ class Nvc < Formula
   homepage "https://github.com/wangsizhu0504/nvc"
   version "0.2.0"
 
-  livecheck do
-    url "https://github.com/wangsizhu0504/nvc/releases/latest"
-    regex(/^(\d{4}-\d{2}-\d{2})$/i)
-    strategy :github_latest
-  end
-
   on_macos do
     url "https://github.com/wangsizhu0504/nvc/releases/latest/download/nvc-macos.zip"
+    sha256 "4796d416e70f3399f1a6c84fcc47dcf97152ab8b78f341b6eb7554b87ac3a4e9"
   end
 
   on_linux do
     url "https://github.com/wangsizhu0504/nvc/releases/latest/download/nvc-linux.zip"
+    sha256 "22c030c5639f760f19ce6d173943715518daaa979d56846675711f3376cc7ea4"
   end
 
   def install
-    bin.install "ma"
+    bin.install "nvc"
   end
 
+  test do
+    assert_match "nvc version #{version}", shell_output("#{bin}/nvc --version")
+  end
 end
